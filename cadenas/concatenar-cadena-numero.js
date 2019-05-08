fe11047d7b6b524c25f03770911b3e73d4c91150
Víctor Cuervo <vcuervo@gmail.com>
2019-05-08 12:38:11 +0200

    Concatenar cadenas y números

---
+++ cadenas/concatenar-cadena-numero.js
@@ -0,0 +1,12 @@
+// Concatenar cadenas
+
+let cadena = '123';
+let numero = 4;
+
+console.log(cadena + numero);
+
+// No concatena, si no que ejecuta la resta
+console.log(cadena - numero);
+
+// Podríamos hacer una suma, restando 0
+console.log(cadena - 0 + numero);
