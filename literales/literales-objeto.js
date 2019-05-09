8de1a4f2b5b5f7d2b1a14ff9b2b760362ebc9761
Víctor Cuervo <vcuervo@gmail.com>
2019-05-09 15:15:59 +0200

    Literales Objetos

---
+++ literales/literales-objeto.js
@@ -0,0 +1,11 @@
+
+// Literales Objetos
+
+var o1 = {};
+console.log(o1);
+
+var o2 = {nombre:'Manual Web'};
+console.log(o2.nombre);
+
+var o3 = {sumar:function suma(a,b){return a+b},nombre:'A'};
+console.log(o3.sumar(2,2));
