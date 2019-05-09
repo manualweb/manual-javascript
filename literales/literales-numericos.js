0717e3d06536cb4c9dfa1b9d245b639296efb91a
Víctor Cuervo <vcuervo@gmail.com>
2019-05-09 13:10:18 +0200

    Literales numéricos.

---
+++ literales/literales-numericos.js
@@ -0,0 +1,16 @@
+
+// Entero
+let n1 = 12;
+console.log(n1);
+
+// Binario
+let n2 = 0B1100;
+console.log(n2);
+
+// Hexadecimal
+let n3 = 0XC;
+console.log(n3);
+
+// Octal
+let n4 = 0O14;
+console.log(n4);
