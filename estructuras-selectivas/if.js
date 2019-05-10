785d4b922dd17ac428a6bbda4b4217fe8ceead8a
Víctor Cuervo <vcuervo@gmail.com>
2019-05-10 12:51:50 +0200

    If para calcuular el mayor de dos números.

---
+++ estructuras-selectivas/if.js
@@ -0,0 +1,9 @@
+let numero1 = 3;
+let numero2 = 7;
+
+if (numero1 > numero2)
+  console.log(numero1 + ' es mayor que ' + numero2);
+else if (numero2 > numero1)
+  console.log(numero2 + ' es mayor que ' + numero1);
+else
+  console.log('Son dos números iguales');
