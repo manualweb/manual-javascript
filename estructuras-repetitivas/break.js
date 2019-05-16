b9b03eaa922480ef7725d70f9665dbcdc32cffc6
Víctor Cuervo <vcuervo@gmail.com>
2019-05-16 17:54:26 +0200

    Break en código.

---
+++ estructuras-repetitivas/break.js
@@ -0,0 +1,16 @@
+
+// Uso de la sentencia break
+
+console.log('--Inicio del Programa--');
+
+let x = 1;
+while (x<=10) {
+
+  if (x==5) {
+    break;
+  }
+  console.log(x);
+  x++;
+}
+
+console.log('--Fin del Programa--');
