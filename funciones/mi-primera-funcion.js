1d85ea0d44ab862bd893cd1bfaab15066eb49e7c
Víctor Cuervo <vcuervo@gmail.com>
2020-05-08 19:38:32 +0200

    Mi primera función.

---
+++ funciones/mi-primera-funcion.js
@@ -0,0 +1,13 @@
+
+
+// Define una primera función que suma dos números
+
+
+function sumar (numero1, numero2) {
+  return numero1 + numero2;
+}
+
+let n1 = 2;
+let n2 = 3;
+
+console.log("La suma es: " + sumar(2,3));
